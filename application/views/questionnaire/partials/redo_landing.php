24e8bf114922f271929bc2316e719f79e37eab6f
Allard van Altena <flythe1@gmail.com>
2017-10-16 11:27:51 +0200

    update redo text

--- application/views/questionnaire/partials/redo_landing.php
+++ application/views/questionnaire/partials/redo_landing.php
@@ -4,7 +4,7 @@
 
     <p>Please help us to gather data on as many tools as possible.</p>
 
-    <p>Answer below whether you want to redo the questionnaire for another tool (<span class="font-weight-bold">this takes approximately 10 minutes</span>).</p>
+    <p>Below, select with which tool you want to redo the questionnaire and click next (<span class="font-weight-bold">this takes approximately 10 minutes</span>).</p>
 
     <select class="form-control">
       <?php foreach ($interesting_software as $software_id => $name) { ?>
